7ffbd3b81babb3adcd93772f093cc9dc9188d32c
Kostiantyn-Klymenko <agent.cook.ok@gmail.com>
2026-02-26 10:58:14 +0400

    git_troubleshoot_4

---
+++ cowbulls_incomplete.py
@@ -0,0 +1,40 @@
+import random
+
+def compare_numbers(number, user_guess):
+    cows = 0
+    bulls = 0
+
+    for i in range(len(user_guess)):
+        if user_guess[i] == number[i]:
+            bulls += 1
+        elif user_guess[i] in number:
+            cows += 1
+
+    return (cows, bulls)
+
+playing = True #gotta play the game
+number = str(random.randint(0,9999)) #random 4 digit number
+guesses = 0
+print(number)
+
+print("Let's play a game of Cowbull!") #explanation
+print("I will generate a number, and you have to guess the numbers one digit at a time.")
+print("For every number that exists in the sequence but is in wrong place, you get a cow. For every one in the right place, you get a bull.")
+print("The game ends when you get 4 bulls!")
+print("Type exit  at any prompt to exit.")
+
+while playing:
+    user_guess = input("Give me your best guess!")
+    if user_guess == "exit":
+        break
+    cowbullcount = compare_numbers(number,user_guess)
+    guesses+=1
+
+    print("You have "+ str(cowbullcount[0]) + " cows, and " + str(cowbullcount[1]) + " bulls.")
+
+    if cowbullcount[1]==4:
+        playing = False
+        print("You win the game after " + str(guesses) + "! The number was "+str(number))
+        break #redundant exit
+    else:
+        print("Your guess isn't quite right, try again.")
